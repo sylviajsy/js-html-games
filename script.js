baa52a23db1db81050ca8b18a4df35dcf993b9c5
Siyi Ji <sylviajsyy@gmail.com>
2025-11-24 22:09:37 -0700

    Official Boggle Dice

---
+++ script.js
@@ -0,0 +1,19 @@
+// Design Boggle Dice
+const BOGGLE_DICE = [
+  "AAEEGN",
+  "ABBJOO",
+  "ACHOPS",
+  "AFFKPS",
+  "AOOTTW",
+  "CIMOTU",
+  "DEILRX",
+  "DELRVY",
+  "DISTTY",
+  "EEGHNW",
+  "EEINSU",
+  "EHRTVW",
+  "EIOSST",
+  "ELRTTY",
+  "HIMNQU",
+  "HLNNRZ"
+];
