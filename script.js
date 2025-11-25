32ecce411a4f6b3795657873b0515be15da21ce9
Siyi Ji <sylviajsyy@gmail.com>
2025-11-24 22:19:46 -0700

    Shuffle the dices

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ const BOGGLE_DICE = [
   "HIMNQU",
   "HLNNRZ"
 ];
+
+// Shuffle the order of 16 dices
+    // Iterate from last index, swap with random previous index
+function shuffle(arr){
+    for (let i=arr.length-1; i>0; i--){
+        // Random index generated
+        const j = Math.floor(Math.random() * (i + 1));
+        // Swap i and j element
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+}
