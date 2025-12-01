ca39dd3a3c792c3127ed1645e9bb93328df3cf93
Siyi Ji <sylviajsyy@gmail.com>
2025-12-01 13:09:44 -0700

    Change variable to state object

--- script.js
+++ script.js
@@ -18,6 +18,18 @@ const BOGGLE_DICE = [
   "HLNNRZ"
 ];
 
+let totalGameTime = 180;
+// State Object
+const GameState = {
+    timeLeft: totalGameTime,
+    score: 0,
+    isTimerActive: false,
+    timerInterval: null,
+    board: [],
+    selectedPath: [],
+    foundWords: new Set()
+};
+
 // Shuffle the order of 16 dices (Fisher-Yates Shuffle)
     // Iterate from last index, swap with random previous index
 function shuffle(arr){
@@ -53,29 +65,30 @@ function generateBoard(){
         }
         board.push(row);
     }
-
+    // Update state
+    GameState.board = board;
     return board;
 }
 
 // Set timer
-let timeLeft = 180;
-let timeInterval;
-let timerStarted = false;
-
 function startTimer() { 
-    timerInterval = setInterval(()=>{
-        timeLeft--;
 
-        const minutes = Math.floor(timeLeft / 60);
-        let seconds = timeLeft % 60;
+    GameState.timerStarted = true;
+
+    GameState.timerInterval = setInterval(()=>{
+        GameState.timeLeft--;
+
+        const minutes = Math.floor(GameState.timeLeft / 60);
+        let seconds = GameState.timeLeft % 60;
         if (seconds < 10) seconds = "0" + seconds;
 
         document.getElementById("timeValue").innerText = `${minutes}:${seconds}`;
 
-        if (timeLeft <=0){
-            clearInterval(timerInterval);
+        if (GameState.GameState.timeLeft <=0){
+            clearInterval(GameState.timerInterval);
             document.getElementById("submitBtn").disabled = true;
             document.querySelectorAll("#board button").forEach(btn => btn.disabled = true);
+            alert("Time is up!");
         }
     }, 1000);
 
@@ -87,8 +100,6 @@ function isNeighbor(r1, c1, r2, c2) {
     const colDiff = Math.abs(c1 - c2);
     return rowDiff <= 1 && colDiff <= 1;
 }
-
-let path = [];
 
 // Render Board
 function renderBoard(board){
@@ -104,17 +115,16 @@ function renderBoard(board){
 
             // Use toggle, color changed back if has bg color
             button.addEventListener('click', (e) => {
-                if (!timerStarted){
+                if (!GameState.isTimerActive){
                     startTimer();
-                    timerStarted = true;
                 }
                 
                 // Only last selected letter could be cancelled
                 if (button.classList.contains("clicked")){
-                    const lastSelected = path[path.length-1];
+                    const lastSelected = GameState.selectedPath[GameState.selectedPath.length-1];
 
                     if (lastSelected.r==r && lastSelected.c==c){
-                        path.pop();
+                        GameState.selectedPath.pop();
                         button.classList.remove("clicked");
                     } else {
                         alert("Only the last selected letter can be unselected")
@@ -123,14 +133,14 @@ function renderBoard(board){
                 }
 
                 // Check if selected letter is adjecent to last letter
-                if (path.length > 0) {
-                    const lastDie = path[path.length - 1];
+                if (GameState.selectedPath.length > 0) {
+                    const lastDie = GameState.selectedPath[pGameState.selectedPath.length - 1];
                     if (!isNeighbor(lastDie.r, lastDie.c, r, c)) {
                         alert("Not adjacent!");
                         return;
                     }
                 }
-                path.push({ r: r, c: c, letter: letter });
+                GameState.selectedPath.push({ r: r, c: c, letter: letter });
                 button.classList.toggle("clicked");
             })
 
@@ -162,23 +172,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const board = generateBoard();
   renderBoard(board);
 
-  let totalScore = 0;
-
   document.getElementById("submitBtn").addEventListener("click", ()=>{
     // A valid word must contain at least 3 letters
-    if (path.length<3){
+    if (GameState.selectedPath.length<3){
         alert("A valid word must contain at least 3 letters");
         return;
     } 
 
-    const word = path.map(p => p.letter).join("");
+    const word = GameState.selectedPath.map(p => p.letter).join("");
     // Check for repeated word
     const existingWord = document.querySelectorAll("#wordList li");
 
     for (let i of existingWord){
         if (i.innerText == word){
             alert("You already found this word!");
-            path = [];
+            GameState.selectedPath = [];
             document.querySelectorAll(".clicked").forEach(btn => {
                 btn.classList.remove("clicked");
             });
@@ -193,15 +201,15 @@ document.addEventListener("DOMContentLoaded", () => {
     wordList.appendChild(newListItem);
 
     // Reset selected letters
-    path = [];
+    GameState.selectedPath = [];
     document.querySelectorAll(".clicked").forEach(btn => {
         btn.classList.remove("clicked");
     });
 
     // Get score
     const score = getScore(word);
-    totalScore += score;
-    document.getElementById("scoreValue").innerText = totalScore;
+    GameState.score += score;
+    document.getElementById("scoreValue").innerText = GameState.score;
   });
 });
 
